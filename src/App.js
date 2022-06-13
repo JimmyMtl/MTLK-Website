63d1507b88c0027b93c1d6f27502f12866ecaeb9
Hades <jimmy.martel@epitech.eu>
2022-06-14 00:04:14 +0200

    removing learn React

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Le site web de MTLK se refait une beauté... Nous revenons bientôt !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
